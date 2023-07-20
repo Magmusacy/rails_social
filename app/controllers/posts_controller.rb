d8d49e3807903000f18b3a4b5fd93b837d582ad4
Magmusacy <xpfk.098@gmail.com>
2023-07-20 18:38:48 +0200

    Add ability to create Posts and use turbo streams to update the index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,24 @@
 class PostsController < ApplicationController
   def index
     @post = Post.new
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    p @post
+    respond_to do |format|
+      if @post.save 
+        format.turbo_stream
+      else
+        redirect_to root_url
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
