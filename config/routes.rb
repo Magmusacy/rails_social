323753a950d3d21345dfca730581ff1525455438
Magmusacy <xpfk.098@gmail.com>
2023-07-21 21:52:38 +0200

    Create user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   resources :posts
+  resources :users, only: [:show, :index]
   resources :comments, only: [:create, :destroy]
   root "posts#index"
 end
