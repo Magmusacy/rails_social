5eaf7e8dc15116f8c44578602d07ea9d505403a4
Magmusacy <xpfk.098@gmail.com>
2023-07-30 17:21:25 +0200

    Add ability to attach images to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,23 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "user_id" 
   has_many :comments, dependent: :destroy
   has_many :likes, as: :likeable, dependent: :destroy
-  
+  has_one_attached :image
+
   validates :body, presence: true
+  validate :acceptable_image
+
+  private
+
+  def acceptable_image
+    return unless image.attached?
+
+    unless image.blob.byte_size <= 1.megabyte
+      errors.add(:image, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(image, "must be a JPEG or PNG")
+    end
+  end
 end
