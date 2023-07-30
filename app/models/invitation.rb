27cf36b0bb43ee60c771eb9973e30c14411f9b23
Magmusacy <xpfk.098@gmail.com>
2023-07-30 20:48:04 +0200

    Add validation to ensure not duplicate invitations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,12 +2,21 @@ class Invitation < ApplicationRecord
     belongs_to :user
     belongs_to :friend, class_name: "User", foreign_key: "friend_id" 
 
-    validates :user_id, uniqueness: { scope: :friend_id }, comparison: { other_than: :friend_id }  
+    validates :user_id, uniqueness: { scope: :friend_id }, comparison: { other_than: :friend_id } 
+    validate :ensure_no_duplicate_invitations
 
     scope :find_invitation, ->(user, friend) { (where(user_id: user).where(friend_id: friend)).or(where(user_id: friend).where(friend_id: user)) }
     scope :confirmed, -> { where(confirmed: true) }
 
     def contains?(user)
         user_id == user.id || friend_id == user.id 
+    end
+
+    private
+
+    def ensure_no_duplicate_invitations
+        unless Invitation.find_invitation(friend_id, user_id).empty?
+            errors.add(:invitation, "already exists")
+        end
     end
 end
