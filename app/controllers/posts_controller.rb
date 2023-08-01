a647b9f37fcd30e08c20ee5dbcd4283dc1bc3d56
Magmusacy <xpfk.098@gmail.com>
2023-08-01 21:56:01 +0200

    Include post images to prevent N + 1 queries

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @post = Post.new
-    @posts = Post.includes(:author, :likes, comments: [:likes, :author, :post]).order(created_at: :desc, "comments.created_at": :desc).all
+    @posts = Post.includes(:likes, :author, comments: [:likes, :post, :author]).order(created_at: :desc, "comments.created_at": :desc).with_attached_image.all
   end
 
   def show
